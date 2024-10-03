eaefdd7ad634b50a0e4e7b59188c1bc1ccb34bdd
Siren <siren@kernal.eu>
2024-10-04 00:50:48 +0300

    Update dependencies and switch to pricefetcher library

--- go.mod
+++ go.mod
@@ -1,13 +1,13 @@
 module gitlab.com/monero-atm/atm-backend
 
-go 1.21.9
+go 1.22.6
 
 require (
 	github.com/eclipse/paho.golang v0.21.0
-	github.com/gorilla/websocket v1.5.1
+	github.com/gorilla/websocket v1.5.3
 	github.com/rs/zerolog v1.33.0
 	gitlab.com/moneropay/go-monero v1.1.1
-	gitlab.com/moneropay/moneropay/v2 v2.5.0
+	gitlab.com/moneropay/moneropay/v2 v2.5.1
 	gitlab.com/openkiosk/proto v0.0.0-20230612142012-deb2b471c26e
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -15,6 +15,7 @@ require (
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
+	github.com/monero-atm/pricefetcher v0.3.0
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 )
--- price.go
+++ price.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 	"time"
 
+	"github.com/monero-atm/pricefetcher"
 	"github.com/rs/zerolog/log"
 )
 
@@ -19,63 +18,31 @@ type priceUpdate struct {
 	Currencies []xmrPrice `json:"currencies"`
 }
 
-var krakenUrl = "https://api.kraken.com/0/public/Ticker?pair=XMR"
-
-type krakenPair struct {
-	Result struct {
-		XmrEur struct {
-			C []string `json:"c"`
-		} `json:"XXMRZEUR"`
-		XmrUsd struct {
-			C []string `json:"c"`
-		} `json:"XXMRZUSD"`
-	} `json:"result"`
-}
-
-func getKrakenRate(currencyShort string) (float64, error) {
-	req, err := http.NewRequest("GET", krakenUrl+currencyShort, nil)
-	if err != nil {
-		return 0, err
-	}
-	cl := &http.Client{Timeout: 5 * time.Second}
-	resp, err := cl.Do(req)
-	if err != nil {
-		return 0, err
-	}
-	var kp krakenPair
-	if err := json.NewDecoder(resp.Body).Decode(&kp); err != nil {
-		return 0, err
-	}
-	var rate string
-	if currencyShort == "EUR" {
-		rate = kp.Result.XmrEur.C[0]
-	} else if currencyShort == "USD" {
-		rate = kp.Result.XmrUsd.C[0]
-	} else {
-		return 0, fmt.Errorf("non-existent XMR pair on Kraken")
-	}
-	return strconv.ParseFloat(rate, 64)
-}
-
-// Kraken only has XMR/EUR and XMR/USD pairs. When another fiat currency is
+// Only XMR/EUR and XMR/USD pairs are available. When another fiat currency is
 // specified, this function will calculate the value based on the daily rate
 // provided by European Central Bank. "fiatEurRate" contains this rate.
 // It's ignored when "currency" is set to EUR or USD.
 func getXmrPrice(currencies []string, fiatRates map[string]float64, fee float64) (priceUpdate, error) {
 	var pu priceUpdate
 
+	cl := &http.Client{Timeout: 5 * time.Second}
+	fetcher := pricefetcher.New(cl)
 	// Get EUR rate
-	eurRate, err := getKrakenRate("EUR")
+	eurRate, source, err := fetcher.FetchXMRPrice("EUR")
 	if err != nil {
 		return pu, err
+	} else {
+		log.Info().Float64("rate", eurRate).Str("source", source).Msg("Got XMR/EUR rate")
 	}
 
 	for _, c := range currencies {
 		var xp xmrPrice
 		if c == "USD" {
-			usdRate, err := getKrakenRate("USD")
+			usdRate, source, err := fetcher.FetchXMRPrice("USD")
 			if err != nil {
 				return pu, err
+			} else {
+				log.Info().Float64("rate", usdRate).Str("source", source).Msg("Got XMR/USD rate")
 			}
 			xp.Amount = usdRate
 			xp.Short = c
